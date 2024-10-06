a8db521ae54c884931a9aa5790d4c5cd8d2baa29
Shuaiqing Luo <luoshuaiqing@gmail.com>
2024-10-06 09:13:16 -0700

    update

--- 15-in-practice-combine-and-swiftui/projects/starter/HNReader/Model/ReaderViewModel.swift
+++ 15-in-practice-combine-and-swiftui/projects/starter/HNReader/Model/ReaderViewModel.swift
@@ -31,24 +31,30 @@
 /// THE SOFTWARE.
 
 import Foundation
+import Combine
 
 class ReaderViewModel {
-  private let api = API()
-  private var allStories = [Story]()
-
-  var filter = [String]()
-  
-  var stories: [Story] {
-    guard !filter.isEmpty else {
-      return allStories
-    }
-    return allStories
-      .filter { story -> Bool in
-        return filter.reduce(false) { isMatch, keyword -> Bool in
-          return isMatch || story.title.lowercased().contains(keyword)
+    private let api = API()
+    private var allStories = [Story]()
+    private var subscriptions = Set<AnyCancellable>()
+    
+    var filter = [String]()
+    
+    var stories: [Story] {
+        guard !filter.isEmpty else {
+            return allStories
         }
-      }
-  }
-  
-  var error: API.Error? = nil
+        return allStories
+            .filter { story -> Bool in
+                return filter.reduce(false) { isMatch, keyword -> Bool in
+                    return isMatch || story.title.lowercased().contains(keyword)
+                }
+            }
+    }
+    
+    var error: API.Error? = nil
+    
+    func fetchStories() {
+        
+    }
 }
