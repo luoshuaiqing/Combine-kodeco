083c91a4c6dc4628840d02de3b33aff88851d3b2
luoshuaiqing <shuaiqin@usc.edu>
2024-10-14 15:25:40 -0700

    update

--- 18-custom-publishers-and-handling-backpressure/projects/Starter.playground/Pages/ShareReplay operator.xcplaygroundpage/Contents.swift
+++ 18-custom-publishers-and-handling-backpressure/projects/Starter.playground/Pages/ShareReplay operator.xcplaygroundpage/Contents.swift
@@ -10,8 +10,8 @@ fileprivate final class ShareReplaySubscription<Output, Failure: Error>: Subscri
     var completion: Subscribers.Completion<Failure>? = nil
     
     
-    init(subscriber: AnySubscriber<Output, Failure>, replay: [Output], capacity: Int, completion: Subscribers.Completion<Failure>?) {
-        self.subscriber = subscriber
+    init<S>(subscriber: S, replay: [Output], capacity: Int, completion: Subscribers.Completion<Failure>?) where S: Subscriber, Failure == S.Failure, Output == S.Input {
+        self.subscriber = AnySubscriber(subscriber)
         self.buffer = replay
         self.capacity = capacity
         self.completion = completion
@@ -66,5 +66,58 @@ fileprivate final class ShareReplaySubscription<Output, Failure: Error>: Subscri
         self.subscriber = nil
         self.buffer.removeAll()
         subscriber.receive(completion: completion)
+    }
+}
+
+extension Publishers {
+    final class ShareReplay<Upstream: Publisher>: Publisher {
+        typealias Output = Upstream.Output
+        typealias Failure = Upstream.Failure
+        
+        private let lock = NSRecursiveLock()
+        private let upstream: Upstream
+        private let capacity: Int
+        private var replay = [Output]()
+        private var subscriptions = [ShareReplaySubscription<Output, Failure>]()
+        private var completion: Subscribers.Completion<Failure>? = nil
+        
+        init(upstream: Upstream, capacity: Int) {
+            self.upstream = upstream
+            self.capacity = capacity
+        }
+        
+        // todo: what's this?
+        private func relay(_ value: Output) {
+            lock.lock()
+            defer { lock.unlock() }
+            
+            guard completion == nil else { return }
+            
+            replay.append(value)
+            if replay.count > capacity {
+                replay.removeFirst()
+            }
+            subscriptions.forEach {
+                $0.receive(value)
+            }
+        }
+        
+        private func complete(_ completion: Subscribers.Completion<Failure>) {
+            lock.lock()
+            defer { lock.unlock() }
+            self.completion = completion
+            subscriptions.forEach {
+                $0.receive(completion: completion)
+            }
+        }
+        
+        func receive<S>(subscriber: S) where S : Subscriber, Upstream.Failure == S.Failure, Upstream.Output == S.Input {
+            lock.lock()
+            defer { lock.unlock() }
+            
+            let subscription = ShareReplaySubscription(subscriber: subscriber, replay: replay, capacity: capacity, completion: completion)
+            subscriptions.append(subscription)
+            subscriber.receive(subscription: subscription)
+        }
     }
 }
