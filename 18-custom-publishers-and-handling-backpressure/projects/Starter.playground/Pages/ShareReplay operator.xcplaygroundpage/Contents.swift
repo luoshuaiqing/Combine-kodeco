6bf4d914e41ab94d37073e5abdc31601827fe4f7
luoshuaiqing <shuaiqin@usc.edu>
2024-10-14 11:24:18 -0700

    update

--- 18-custom-publishers-and-handling-backpressure/projects/Starter.playground/Pages/ShareReplay operator.xcplaygroundpage/Contents.swift
+++ 18-custom-publishers-and-handling-backpressure/projects/Starter.playground/Pages/ShareReplay operator.xcplaygroundpage/Contents.swift
@@ -1,39 +1,70 @@
 import Foundation
 import Combine
 
-<#Add your code here#>
+fileprivate final class ShareReplaySubscription<Output, Failure: Error>: Subscription {
 
-//: [Next](@next)
-/*:
- Copyright (c) 2023 Kodeco Inc.
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
-
- Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- distribute, sublicense, create a derivative work, and/or sell copies of the
- Software in any work that is designed, intended, or marketed for pedagogical or
- instructional purposes related to programming, coding, application development,
- or information technology.  Permission for such use, copying, modification,
- merger, publication, distribution, sublicensing, creation of derivative works,
- or sale is expressly withheld.
-
- This project and source code may use libraries or frameworks that are
- released under various Open-Source licenses. Use of those libraries and
- frameworks are governed by their own individual licenses.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- */
+    let capacity: Int
+    var subscriber: AnySubscriber<Output, Failure>? = nil
+    var demand: Subscribers.Demand = .none
+    var buffer: [Output]
+    var completion: Subscribers.Completion<Failure>? = nil
+    
+    
+    init(subscriber: AnySubscriber<Output, Failure>, replay: [Output], capacity: Int, completion: Subscribers.Completion<Failure>?) {
+        self.subscriber = subscriber
+        self.buffer = replay
+        self.capacity = capacity
+        self.completion = completion
+    }
+    
+    private func complete(with completion: Subscribers.Completion<Failure>) {
+        guard let subscriber else { return }
+        self.subscriber = nil
+        self.completion = nil
+        self.buffer.removeAll()
+        subscriber.receive(completion: completion)
+    }
+    
+    private func emitAsNeeded() {
+        guard let subscriber else { return }
+        while demand > .none && !buffer.isEmpty {
+            demand -= .max(1)
+            let nextDemand = subscriber.receive(buffer.removeFirst())
+            if nextDemand != .none {
+                self.demand += nextDemand
+            }
+        }
+        if let completion {
+            complete(with: completion)
+        }
+    }
+    
+    func request(_ demand: Subscribers.Demand) {
+        if demand != .none {
+            self.demand += demand
+        }
+        emitAsNeeded()
+    }
+    
+    func cancel() {
+        complete(with: .finished)
+    }
+    
+    // todo: what's this?
+    func receive(_ input: Output) {
+        guard subscriber != nil else { return }
+        buffer.append(input)
+        if buffer.count > capacity {
+            buffer.removeFirst()
+        }
+        emitAsNeeded()
+    }
+    
+    // todo: what's this?
+    func receive(completion: Subscribers.Completion<Failure>) {
+        guard let subscriber else { return }
+        self.subscriber = nil
+        self.buffer.removeAll()
+        subscriber.receive(completion: completion)
+    }
+}
