ec76b22329a9228ede420c0ee4b5ffba3fb55fc8
luoshuaiqing <shuaiqin@usc.edu>
2024-10-18 23:21:28 -0700

    update

--- 20-in-practice-building-a-complete-app/projects/starter/ChuckNorrisJokesModel/View Models/JokesViewModel.swift
+++ 20-in-practice-building-a-complete-app/projects/starter/ChuckNorrisJokesModel/View Models/JokesViewModel.swift
@@ -43,7 +43,7 @@ public final class JokesViewModel {
     
     @Published public var fetching = false
     @Published public var joke = Joke.starter
-    @Published public var backgroundColor = Color.gray
+    @Published public var backgroundColor = Color("Gray")
     @Published public var decisionState = DecisionState.undecided
     
     private let jokesService: JokeServiceDataPublisher
@@ -53,27 +53,47 @@ public final class JokesViewModel {
         
         $joke
             .map { _ in
-                 false
+                false
             }
             .assign(to: &$fetching)
     }
     
     public func fetchJoke() {
-        
+        fetching = true
+        jokesService.publisher()
+            .retry(1)
+            .decode(type: Joke.self, decoder: Self.decoder)
+            .replaceError(with: Joke.error)
+            .receive(on: DispatchQueue.main)
+            .assign(to: &$joke)
     }
     
     public func updateBackgroundColorForTranslation(_ translation: Double) {
-        
+        switch translation {
+        case ...(-0.5):
+            backgroundColor = Color("Red")
+        case 0.5...:
+            backgroundColor = Color("Green")
+        default:
+            backgroundColor = Color("Gray")
+        }
     }
     
     public func updateDecisionStateForTranslation(
         _ translation: Double,
         andPredictedEndLocationX x: CGFloat,
         inBounds bounds: CGRect) {
-            
+            switch (translation, x) {
+            case (...(-0.6), ..<0):
+                decisionState = .disliked
+            case (0.6..., bounds.width...):
+                decisionState = .liked
+            default:
+                decisionState = .undecided
+            }
         }
     
     public func reset() {
-        
+        backgroundColor = Color("Gray")
     }
 }
