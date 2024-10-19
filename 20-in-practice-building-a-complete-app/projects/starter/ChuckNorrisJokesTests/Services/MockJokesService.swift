d49e03a993e16c6f73da113aef4fbabaa9e6d5e2
luoshuaiqing <shuaiqin@usc.edu>
2024-10-18 18:34:56 -0700

    update

--- 20-in-practice-building-a-complete-app/projects/starter/ChuckNorrisJokesTests/Services/MockJokesService.swift
+++ 20-in-practice-building-a-complete-app/projects/starter/ChuckNorrisJokesTests/Services/MockJokesService.swift
@@ -35,13 +35,23 @@ import Combine
 @testable import ChuckNorrisJokesModel
 
 struct MockJokesService: JokeServiceDataPublisher {
-  let data: Data
-  let error: URLError?
-  
-  init(data: Data, error: URLError? = nil) {
-    self.data = data
-    self.error = error
-  }
-  
-  
+    let data: Data
+    let error: URLError?
+    
+    init(data: Data, error: URLError? = nil) {
+        self.data = data
+        self.error = error
+    }
+    
+    func publisher() -> AnyPublisher<Data, URLError> {
+        let publisher = CurrentValueSubject<Data, URLError>(data)
+        DispatchQueue.global().asyncAfter(deadline: .now() + 0.1) {
+            if let error {
+                publisher.send(completion: .failure(error))
+            } else {
+                publisher.send(data)
+            }
+        }
+        return publisher.eraseToAnyPublisher()
+    }
 }
