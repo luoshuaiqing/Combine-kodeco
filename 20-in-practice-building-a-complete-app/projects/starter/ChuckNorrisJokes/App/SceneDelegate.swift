457eca0faa3154f217f8e82310ea871787e158bd
luoshuaiqing <shuaiqin@usc.edu>
2024-11-04 10:40:40 -0600

    update

--- 20-in-practice-building-a-complete-app/projects/starter/ChuckNorrisJokes/App/SceneDelegate.swift
+++ 20-in-practice-building-a-complete-app/projects/starter/ChuckNorrisJokes/App/SceneDelegate.swift
@@ -34,24 +34,24 @@ import UIKit
 import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  var window: UIWindow?
-  
-  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    let contentView = JokeView()
+    var window: UIWindow?
     
-    if let windowScene = scene as? UIWindowScene {
-      let window = UIWindow(windowScene: windowScene)
-      window.rootViewController = UIHostingController(rootView: contentView)
-      self.window = window
-      window.makeKeyAndVisible()
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let contentView = JokeView()
+        
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
-  }
-
-  func sceneDidEnterBackground(_ scene: UIScene) {
-    // Called as the scene transitions from the foreground to the background.
-    // Use this method to save data, release shared resources, and store enough scene-specific state information
-    // to restore the scene back to its current state.
     
-    // Save changes in the application's managed object context when the application transitions to the background.
-  }
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+        
+        // Save changes in the application's managed object context when the application transitions to the background.
+    }
 }
