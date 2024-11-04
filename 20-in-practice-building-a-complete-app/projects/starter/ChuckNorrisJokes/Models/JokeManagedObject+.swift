a8c96afc5ccd7a5f4f05d1198fd0714800278db5
luoshuaiqing <shuaiqin@usc.edu>
2024-11-04 10:32:02 -0600

    update

---
+++ 20-in-practice-building-a-complete-app/projects/starter/ChuckNorrisJokes/Models/JokeManagedObject+.swift
@@ -0,0 +1,51 @@
+//
+//  JokeManagedObject+.swift
+//  ChuckNorrisJokes
+//
+//  Created by Shuaiqing Luo on 10/26/24.
+//  Copyright © 2024 Scott Gardner. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import CoreData
+import ChuckNorrisJokesModel
+
+extension JokeManagedObject {
+    static func save(joke: Joke, inViewContext viewContext: NSManagedObjectContext) {
+        guard joke.id != "error" else { return }
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: String(describing: JokeManagedObject.self)) // todo: does entityName matter?
+        fetchRequest.predicate = NSPredicate(format: "id = %@", joke.id)
+        
+        if let results = try? viewContext.fetch(fetchRequest),
+            let existing = results.first as? JokeManagedObject {
+            existing.value = joke.value
+            existing.categories = joke.categories as NSArray
+        } else {
+            let newJoke = self.init(context: viewContext)
+            newJoke.id = joke.id
+            newJoke.value = joke.value
+            newJoke.categories = joke.categories as NSArray
+        }
+        
+        do {
+            try viewContext.save() // todo: do I have to save it?
+        } catch {
+            fatalError()
+        }
+    }
+}
+
+extension Collection where Element == JokeManagedObject, Index == Int {
+    func delete(at indices: IndexSet, inViewContext viewContext: NSManagedObjectContext) {
+        indices.forEach { index in
+            viewContext.delete(self[index])
+        }
+        
+        do {
+            try viewContext.save()
+        } catch {
+            fatalError("\(#file), \(#function), \(error.localizedDescription)")
+        }
+    }
+}
